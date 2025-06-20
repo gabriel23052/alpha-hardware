de7c8d6ce9535f2cce1b89546ae8555d42f17f73
Gabriel Pereira <gabrielggp2002@gmail.com>
2025-06-20 15:06:07 -0300

    Adiciona o react-router ao projeto e cria a rota index

--- src/components/App.tsx
+++
@@ -1,8 +0,0 @@
-function App() {
-  return (
-    <div className="bg-primary dneutral-dark text-display">Hello world!</div>
-  );
-}
-
-export default App;
-
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./main.css";
-import App from './components/App.tsx'
+
+import { BrowserRouter, Routes, Route } from "react-router";
+import Index from "./pages/Index";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )

c1a3e9cea98c397384c096f9d1401496a64f70b8
Gabriel Pereira <gabrielggp2002@gmail.com>
2025-06-23 14:36:40 -0300

    Ajusta o Header para ficar fixo no topo da tela

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,15 @@
 const Index = () => {
-  return <div>Main page</div>;
+  return (
+    <div
+      style={{
+        height: "2000px",
+        backgroundColor: "#caa3a3",
+        paddingTop: "0px",
+      }}
+    >
+      Main page
+    </div>
+  );
 };
 
 export default Index;

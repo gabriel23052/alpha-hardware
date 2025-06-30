e7fe82c7e03b7c901f4ee2488ab31f5eb16bd0d4
Gabriel Pereira <gabrielggp2002@gmail.com>
2025-06-30 10:56:03 -0300

    Adiciona dados de um produto de forma improvisada

---
+++ src/data/product.ts
@@ -0,0 +1,15 @@
+const product: IProduct = {
+  id: "0cb98cb8e",
+  name: "Processador Intel Core i7-12700K, 3.6GHz (5.0GHz Max Turbo), 12 Núcleos, 20 Threads, LGA 1700, Vídeo Integrado",
+  price: 174999,
+  cardPrice: 194443,
+  maxInstallments: 12,
+  installmentsPrice: 16203,
+  cashDiscount: 10,
+  sale: null,
+  imagesSrc: {
+    thumb: "./img/products/0cb98cb8e-0.jpg",
+  }
+};
+
+export default product;
--- src/types.d.ts
+++ src/types.d.ts
@@ -10,3 +10,25 @@ interface IResponsiveBanner {
     src: string;
   }[];
 }
+
+interface IProduct {
+  id: string;
+  name: string;
+  price: number;
+  cardPrice: number;
+  maxInstallments: number;
+  installmentsPrice: number;
+  cashDiscount: number;
+  sale: null | {
+    discount: number;
+    expiresAt: number;
+    price: number;
+    cardPrice: number;
+    maxInstallments: number;
+    installmentsPrice: number;
+    oldPrice: number;
+  };
+  imagesSrc: {
+    thumb: string;
+  };
+}

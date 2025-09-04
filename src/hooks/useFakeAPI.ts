15421d735bc96702c100822e149f70457de1e501
Gabriel Pereira <gabrielggp2002@gmail.com>
2025-09-04 15:50:36 -0300

    Adiciona uma nova função para fazer o fetch na fakeAPI

--- src/hooks/useFakeAPI.ts
+++ src/hooks/useFakeAPI.ts
@@ -1,32 +1,23 @@
 import { useState } from "react";
 
 import endpoints from "@fakeAPI/endpoints";
+import fakeFetch from "@utils/fakeFetch";
 
 export default function useFakeAPI<T>(route: keyof typeof endpoints) {
   const [data, setData] = useState<null | T>(null);
   const [error, setError] = useState<null | string>(null);
   const [loading, setLoading] = useState(false);
-  const routeHandler = endpoints[route];
 
-  const MAX_RESPONSE_TIME = 3000;
-  const MIN_RESPONSE_TIME = 1000;
-
-  function request(params?: Record<string, unknown>) {
+  async function request(params?: Record<string, unknown>) {
     setLoading(true);
-    if (routeHandler === undefined) {
-      setError("Rota não encontrada");
+    const response = await fakeFetch(route, params);
+    if (response.error !== null) {
+      setError(response.error);
+      setLoading(false);
       return;
     }
-    setTimeout(() => {
-      const response = routeHandler(params || {});
-      if (response.error !== null) {
-        setError(response.error);
-        setLoading(false);
-        return;
-      }
-      setData(response.data as T);
-      setLoading(false);
-    }, Math.floor(Math.random() * MAX_RESPONSE_TIME) + MIN_RESPONSE_TIME);
+    setData(response.data as T);
+    setLoading(false);
   }
 
   return { data, error, loading, request };
---
+++ src/utils/fakeFetch.ts
@@ -0,0 +1,18 @@
+import endpoints from "@fakeAPI/endpoints";
+
+export default async function fakeFetch<T>(
+  route: keyof typeof endpoints,
+  params?: Record<string, unknown>
+): Promise<IFakeApiResponse<T>> {
+  const MAX_RESPONSE_TIME = 3000;
+  const MIN_RESPONSE_TIME = 1000;
+  const routeHandler = endpoints[route];
+  return new Promise((resolve) => {
+    if (!routeHandler) {
+      resolve({ data: null, error: "Rota não encontrada" });
+    }
+    setTimeout(() => {
+      resolve(routeHandler(params || {}) as IFakeApiResponse<T>);
+    }, Math.floor(Math.random() * MAX_RESPONSE_TIME) + MIN_RESPONSE_TIME);
+  });
+}

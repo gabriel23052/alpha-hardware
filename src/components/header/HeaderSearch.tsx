d7c72b11952b26c39d741327cca5bff0974032c2
Gabriel Pereira <gabrielggp2002@gmail.com>
2025-09-13 11:13:56 -0300

    Adiciona features de UX e feedback no componente HeaderSearch
    - Feedback de erros e carregamento
    - Ajuste no HTML e CSS para comportar os novos estados
    - Agora o ESC fecha a busca
    - Adiciona ID's para evitar chamadas duplicadas da API

--- src/components/header/HeaderSearch.tsx
+++ src/components/header/HeaderSearch.tsx
@@ -13,11 +13,14 @@ const BLUR_DELAY = 100;
 
 const HeaderSearch = () => {
   const [search, setSearch] = useState("");
+  const [error, setError] = useState<null | string>(null);
+  const [loading, setLoading] = useState(false);
   const [suggestionsShown, setSuggestionsShown] = useState<
     IProductSuggestion[]
   >([]);
 
   const searchStateRef = useRef("");
+  const fetchID = useRef(0);
   const prevSearchLength = useRef(0);
   const suggestions = useRef<IProductSuggestion[]>([]);
   const focused = useRef(false);
@@ -28,7 +31,14 @@ const HeaderSearch = () => {
   const suggestionsSearch = useRef(
     debounce((searchInApi: boolean) => {
       if (searchInApi) {
-        fetchSuggestions();
+        if (
+          focused.current &&
+          searchStateRef.current.length >= MIN_SEARCH_LENGTH
+        ) {
+          setSuggestionsShown([]);
+          setError(null);
+          fetchSuggestions();
+        }
         return;
       }
       setSuggestionsShown(
@@ -47,6 +57,8 @@ const HeaderSearch = () => {
     searchStateRef.current = search;
     if (search.length < MIN_SEARCH_LENGTH) {
       setSuggestionsShown([]);
+      setLoading(false);
+      setError(null);
       suggestions.current = [];
       return;
     }
@@ -55,6 +67,7 @@ const HeaderSearch = () => {
       suggestions.current.length === 0
     ) {
       suggestions.current = [];
+      fetchID.current++;
       suggestionsSearch.current(true);
       return;
     }
@@ -79,6 +92,8 @@ const HeaderSearch = () => {
     blurTimeout.current = window.setTimeout(() => {
       if (!focused.current) {
         setSuggestionsShown([]);
+        setLoading(false);
+        setError(null);
       }
     }, BLUR_DELAY);
   };
@@ -96,6 +111,11 @@ const HeaderSearch = () => {
   };
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Escape") {
+      setError(null);
+      setLoading(false);
+      setSuggestionsShown([]);
+    }
     if (e.currentTarget instanceof HTMLInputElement && e.key === "ArrowDown") {
       e.preventDefault();
       linksElem.current[0]?.focus();
@@ -118,19 +138,26 @@ const HeaderSearch = () => {
   };
 
   const fetchSuggestions = async () => {
+    const currentFetchID = fetchID.current;
+    setLoading(true);
     const response = await fakeFetch<IProductSuggestion[]>(
       "GET /api/products/suggestions",
       { search: searchStateRef.current }
     );
-    if (response.error) return;
-    console.log(searchStateRef.current.length);
-    if (
-      response.data &&
-      focused.current &&
-      searchStateRef.current.length >= MIN_SEARCH_LENGTH
-    ) {
+    if (currentFetchID !== fetchID.current) {
+      return;
+    }
+    setLoading(false);
+    if (!focused.current || searchStateRef.current.length < MIN_SEARCH_LENGTH)
+      return;
+    if (response.error) {
+      setError("Erro ao buscar sugestões de produtos");
+      setSuggestionsShown([]);
+    }
+    if (response.data) {
       suggestions.current = response.data;
       prevSearchLength.current = searchStateRef.current.length;
+      setError(null);
       setSuggestionsShown(suggestions.current.slice(0, SUGGESTIONS_LIMIT));
     }
   };
@@ -142,8 +169,12 @@ const HeaderSearch = () => {
       onBlur={handleBlur}
     >
       <input
-        className={`text-default dneutral-dark ${classes.input} ${
-          suggestionsShown.length > 0 ? classes.withSuggestions : ""
+        className={`text-default dneutral-dark bg-lneutral-light ${
+          classes.input
+        } ${
+          suggestionsShown.length > 0 || error || loading
+            ? classes.withSuggestions
+            : ""
         }`}
         type="text"
         role="combobox"
@@ -157,34 +188,43 @@ const HeaderSearch = () => {
         onPaste={handlePaste}
         onKeyDown={handleKeyDown}
       />
-      {suggestionsShown.length > 0 && (
-        <ul
-          className={`bg-lneutral-xlight ${classes.suggestions}`}
-          id="suggestion-list"
-          role="listbox"
-        >
-          {suggestionsShown.map((item, index) => (
-            <li
-              className={`text-default dneutral-dark ${classes.suggestionItem}`}
-              key={item.id}
-              role="option"
-              data-index={index}
-              onKeyDown={handleKeyDown}
-            >
-              <Link
-                to={`/product/${item.id}`}
+      <div
+        className={`bg-lneutral-xlight ${classes.suggestionsContainer} ${
+          loading ? classes.loading : ""
+        }`}
+      >
+        {error !== null && (
+          <div className={`primary text-default ${classes.error}`}>{error}</div>
+        )}
+        {suggestionsShown.length > 0 && (
+          <ul
+            className={`${classes.suggestions}`}
+            id="suggestion-list"
+            role="listbox"
+          >
+            {suggestionsShown.map((item, index) => (
+              <li
+                className={`text-default dneutral-dark ${classes.suggestionItem}`}
+                key={item.id}
+                role="option"
                 data-index={index}
-                tabIndex={-1}
-                ref={(el) => {
-                  linksElem.current[index] = el!;
-                }}
+                onKeyDown={handleKeyDown}
               >
-                {item.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
+                <Link
+                  to={`/product/${item.id}`}
+                  data-index={index}
+                  tabIndex={-1}
+                  ref={(el) => {
+                    linksElem.current[index] = el!;
+                  }}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };

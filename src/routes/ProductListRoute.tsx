b143d06234e3320a05c96b6a360cd7099fed82f0
Gabriel Pereira <gabrielggp2002@gmail.com>
2025-10-16 14:27:06 -0300

    Adiciona feedback visual e controle para o filtro de promoções
    - Adiciona uma etiqueta com o nome da promoção filtrada
    - Agora os cards dos produtos apresentam detalhes da promoção quando filtrados por promoção
    - Ajusta o state de filtro para assumir um valor inicial baseado nos parâmetros da URL

--- src/routes/ProductListRoute.tsx
+++ src/routes/ProductListRoute.tsx
@@ -1,18 +1,22 @@
 import { useEffect, useState } from "react";
-import { useLocation, useSearchParams } from "react-router";
+import { useSearchParams } from "react-router";
 
 import ProductFilter from "@components/product/ProductFilter";
 import ProductList from "@components/product/ProductList";
 
 import useFakeAPI from "@hooks/useFakeAPI";
 
+import SVGClose from "@svg/close.svg?react";
+
 import classes from "./ProductListRoute.module.css";
 
 const ProductListRoute = () => {
-  const [filter, setFilter] = useState<IFakeApiProductFilter>({});
   const [params] = useSearchParams();
-  const location = useLocation();
-
+  const [filter, setFilter] = useState<IFakeApiProductFilter>(() => {
+    const sale = params.get("sale");
+    if (sale && sale.length === 6) return { sale };
+    return {};
+  });
   const {
     data: productGroup,
     loading,
@@ -20,33 +24,48 @@ const ProductListRoute = () => {
     request,
   } = useFakeAPI<IProductGroup>("GET /api/products");
 
-  useEffect(() => {
-    const sale = params.get("sale");
-    if (sale && sale.length === 6) setFilter((prev) => ({ ...prev, sale }));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location]);
+  const products = productGroup?.products;
+  const meta = productGroup?.meta;
 
   useEffect(() => {
-    if (Object.keys(filter).length === 0) return;
     request(filter);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filter]);
 
+  const removeSaleFilter = () => {
+    setFilter((prev) => {
+      const newFilter = { ...prev };
+      delete newFilter.sale;
+      return newFilter;
+    });
+  };
+
   return (
     <main className={`defaultContainer ${classes.productListRoute}`}>
-      <div className={`bg-dneutral ${classes.decoration}`}></div>
       <ProductFilter setFilter={setFilter} />
       {loading ? (
         <h1>Carregando</h1>
       ) : error ? (
         <h1>Erro: {error}</h1>
       ) : (
-        productGroup && (
-          <ProductList
-            className={classes.productList}
-            products={productGroup.products}
-            hideSale={true}
-          />
+        products && (
+          <>
+            {meta?.saleName && (
+              <span
+                className={`bg-secondary-light secondary-xdark text-default ${classes.saleName}`}
+              >
+                {meta?.saleName}{" "}
+                <button onClick={removeSaleFilter}>
+                  <SVGClose />
+                </button>
+              </span>
+            )}
+            <ProductList
+              className={classes.productList}
+              products={products}
+              hideSale={!meta?.saleName}
+            />
+          </>
         )
       )}
     </main>

ec2d40ff65e01efba5a6536572d8d645466880a3
Gabriel Pereira <gabrielggp2002@gmail.com>
2025-11-06 10:31:09 -0300

    Refatora a forma como o Header se comporta no mobile
    - Altera o botão que controla o menu, o colocando dentro do header
    - Altera a forma como as animações são feitas, sendo agora controladas via atributo style no elemento + transição no css
    - Altera a forma como os elementos são omitidos, sendo escondidos via overflow: hidden
    - Adiciona comportamento de logo omitindo ao scrollar a página

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,6 +5,8 @@ import HeaderUserLinks from "./HeaderUserLinks";
 import HeaderNavBar from "./HeaderNavBar";
 import HeaderSearch from "./HeaderSearch";
 
+import useDebounce from "@hooks/useDebounce";
+
 import SVGLogo from "@svg/logo.svg?react";
 import SVGFavorite from "@svg/favorite.svg?react";
 import SVGMyPurchases from "@svg/myPurchases.svg?react";
@@ -17,21 +19,42 @@ const Header = () => {
   const [showAllHeaderContent, setShowAllHeaderContent] = useState(
     window.innerWidth > 700
   );
+  const [showLogo, setShowLogo] = useState(true);
 
   const mediaQuery = useRef(window.matchMedia("(max-width: 700px)"));
-  const mediaQueryCallback = useRef(() => setShowAllHeaderContent(true));
+  const mediaQueryCallback = useRef(() => {
+    setShowLogo(true);
+    setShowAllHeaderContent(true);
+  });
+  const scrollCallback = useRef(
+    useDebounce(() => {
+      if (!mediaQueryMatch()) return;
+      if (window.scrollY === 0) {
+        setShowLogo(true);
+        return;
+      }
+      setShowLogo(false);
+    }, 100)
+  );
 
   useEffect(() => {
     mediaQuery.current.addEventListener("change", mediaQueryCallback.current);
+    window.addEventListener("scroll", scrollCallback.current);
     return () => {
       // eslint-disable-next-line react-hooks/exhaustive-deps
       mediaQuery.current.removeEventListener(
         "change",
+        mediaQueryCallback.current
+      );
+      window.removeEventListener(
+        "scroll",
         // eslint-disable-next-line react-hooks/exhaustive-deps
         mediaQueryCallback.current
       );
     };
   }, []);
+
+  const mediaQueryMatch = () => mediaQuery.current.matches;
 
   const handleClick = () => {
     setShowAllHeaderContent((prev) => !prev);
@@ -40,44 +63,54 @@ const Header = () => {
   return (
     <header className={`${classes.headerContainer}`}>
       <div
-        className={`${classes.overflowContainer}  ${
-          showAllHeaderContent ? classes.allContent : ""
-        }`}
+        className={`${classes.overflowContainer}`}
+        style={{
+          maxHeight: showAllHeaderContent
+            ? showLogo
+              ? "9.5rem"
+              : "6.3rem"
+            : "33rem",
+        }}
       >
         <div className={`bg-primary`}>
           <div className={`defaultContainer ${classes.wrapper}`}>
-            <Link className={`${classes.logo}`} to="/">
+            <Link
+              className={`${classes.logo}`}
+              to="/"
+              style={{
+                maxHeight: showLogo ? "4rem" : "0px",
+                opacity: showLogo ? "1" : "0",
+              }}
+            >
               <SVGLogo />
             </Link>
+            <button
+              className={`${classes.mobileMenuBtn}`}
+              aria-expanded={showAllHeaderContent ? "true" : "false"}
+              aria-controls="ariaHeaderUserLinks ariaShortcuts ariaHeaderNavBar"
+              onClick={handleClick}
+            >
+              <SVGMenu />
+            </button>
             <HeaderSearch />
-            {showAllHeaderContent && (
-              <>
-                <HeaderUserLinks />
-                <nav className={`${classes.shortcuts}`} id="ariaShortcuts">
-                  <Link to="/">
-                    <SVGFavorite />
-                  </Link>
-                  <Link to="/">
-                    <SVGMyPurchases />
-                  </Link>
-                  <Link to="/">
-                    <SVGCart />
-                  </Link>
-                </nav>
-              </>
-            )}
+            <>
+              <HeaderUserLinks />
+              <nav className={`${classes.shortcuts}`} id="ariaShortcuts">
+                <Link to="/">
+                  <SVGFavorite />
+                </Link>
+                <Link to="/">
+                  <SVGMyPurchases />
+                </Link>
+                <Link to="/">
+                  <SVGCart />
+                </Link>
+              </nav>
+            </>
           </div>
         </div>
-        {showAllHeaderContent && <HeaderNavBar />}
+        <HeaderNavBar />
       </div>
-      <button
-        className={`bg-dneutral ${classes.mobileMenuBtn}`}
-        aria-expanded={showAllHeaderContent ? "true" : "false"}
-        aria-controls="ariaHeaderUserLinks ariaShortcuts ariaHeaderNavBar"
-        onClick={handleClick}
-      >
-        <SVGMenu />
-      </button>
     </header>
   );
 };

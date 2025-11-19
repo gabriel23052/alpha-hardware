257ac127998b28ec7efee0a3c7f796fc95641948
Gabriel Pereira <gabrielggp2002@gmail.com>
2025-11-19 14:16:07 -0300

    Refatora o hook "useDebounce"
    - A função de callback retornada agora é memoizada com useCallback, evitando recriações desnecessárias
    - Adiciona uma função de cleanup ao hook, evitando vazamentos de memória

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,12 +1,24 @@
-import { useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 export default function useDebounce<T extends unknown[]>(
   func: (...args: T) => unknown,
   delay: number
 ) {
-  const timeout = useRef<number | undefined>(undefined);
-  return (...args: T) => {
-    clearTimeout(timeout.current);
-    timeout.current = window.setTimeout(() => func(...args), delay);
-  };
+  const timeoutRef = useRef<number | undefined>(undefined);
+
+  const debouncedFunction = useCallback(
+    (...args: T) => {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = window.setTimeout(() => func(...args), delay);
+    },
+    [func, delay]
+  );
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
+  }, []);
+
+  return debouncedFunction;
 }

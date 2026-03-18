b55256f2fa13b8ae5bf1c2c2c216b63aacbaf4b0
Gabriel <gabrielggp2002@gmail.com>
2026-03-18 14:57:58 -0300

    Refatora os handlers
    - Atualiza os handlers de forma que agora eles não acessam os dados de forma direta, e sim através das classes "table"
    - Atualiza o handler de conteúdo de página para ser focado na página inicial, o retorno de produtos para a interface será revisado futuramente

---
+++ src/fakeAPI/handlers/HomepageHandler.ts
@@ -0,0 +1,49 @@
+import { config } from "@fakeAPI/config";
+
+import { ErrorMessages } from "@fakeAPI/ErrorMessages";
+import { FakeAPIResponse } from "@fakeAPI/FakeAPIResponse";
+
+import { BannersTable } from "@fakeAPI/tables/BannersTable";
+import { CollectionsTable } from "@fakeAPI/tables/CollectionsTable";
+import { SalesTable } from "@fakeAPI/tables/SalesTable";
+
+class HomepageHandler {
+  public getHomepageBanners(response: FakeAPIResponse<FAHomepageBanners_Full>) {
+    const bannersTable = new BannersTable();
+    bannersTable.searchById(config.homepage.saleBannerId);
+    if (bannersTable.empty)
+      return response.setError(ErrorMessages.HP_SALE_BANNER_NOT_FOUND);
+    const saleBanner = bannersTable.getInFullFormat()[0];
+    bannersTable.searchById(config.homepage.adBannerId);
+    if (bannersTable.empty)
+      return response.setError(ErrorMessages.HP_AD_BANNER_NOT_FOUND);
+    const adBanner = bannersTable.getInFullFormat()[0];
+    response.setData({ sale: saleBanner, ad: adBanner });
+  }
+
+  public getHomepageSale(response: FakeAPIResponse<FASale_PrCard>) {
+    const salesTable = new SalesTable();
+    salesTable.searchById(config.homepage.saleId);
+    if (salesTable.empty)
+      return response.setError(ErrorMessages.HP_SALE_NOT_FOUND);
+    return salesTable.getInPrCardFormat()[0];
+  }
+
+  public getHomepageCollections(
+    response: FakeAPIResponse<FAHomepageCollections_Full>,
+  ) {
+    const collectionsTable = new CollectionsTable();
+    const { firstCollectionId, secondCollectionId } = config.homepage;
+    collectionsTable.searchById(firstCollectionId);
+    if (collectionsTable.empty)
+      return response.setError(ErrorMessages.HP_FIRST_COLLECTION_NOT_FOUND);
+    const first = collectionsTable.getInPrCardFormat()[0];
+    collectionsTable.searchById(secondCollectionId);
+    if (collectionsTable.empty)
+      return response.setError(ErrorMessages.HP_FIRST_COLLECTION_NOT_FOUND);
+    const second = collectionsTable.getInPrCardFormat()[0];
+    response.setData({ first, second });
+  }
+}
+
+export { HomepageHandler };
--- src/fakeAPI/handlers/ProductsHandler.ts
+++ src/fakeAPI/handlers/ProductsHandler.ts
@@ -1,188 +1,71 @@
-import exclusionFilter from "@utils/exclusionFilter";
+import type { FakeAPIResponse } from "@fakeAPI/FakeAPIResponse";
+import { ProductsTable } from "@fakeAPI/tables/ProductsTable";
 
-import products from "@fakeAPI/data/products";
-import sales from "@fakeAPI/data/sales";
-
-const MAX_PRICE = 9999999;
-const RELATED_ARRAY_MAX_LENGTH = 4;
-
-export default class ProductsHandler {
-  private productsMap: Map<string, IProduct>;
-  private productsSaleMap: Map<string, { sale: IProductSale; prices: IPrices }>;
-
-  constructor() {
-    this.productsMap = new Map();
-    products.forEach((product) => {
-      this.productsMap.set(product.id, product);
-    });
-
-    this.productsSaleMap = new Map();
-    sales.forEach((sale) => {
-      sale.products.forEach((product) => {
-        this.productsSaleMap.set(product.id, {
-          sale: {
-            id: sale.id,
-            name: sale.name,
-            discont: product.discont,
-            expiration: product.expiration,
-          },
-          prices: product.prices,
-        });
-      });
-    });
+class ProductsHandler {
+  public getProductById(
+    response: FakeAPIResponse<FAProduct | null>,
+    productId: string,
+  ) {
+    const productsTable = new ProductsTable();
+    productsTable.searchById(productId);
+    if (productsTable.empty) {
+      return response.setData(null);
+    }
+    return response.setData(productsTable.getInFullFormat()[0]);
   }
 
-  private getProducts(filter: IFakeApiProductFilter) {
-    const result: IProduct[] = [];
-
-    if ("id" in filter) {
-      if (typeof filter.id === "string") filter.id = [filter.id];
-      filter.id?.forEach((id) => {
-        const product = this.productsMap.get(id);
-        if (product) result.push(product);
-      });
-      return result;
-    }
-
-    if ("name" in filter) {
-      const filterName = (filter.name as string).toLowerCase();
-      result.push(
-        ...products.filter((product) =>
-          product.name.toLowerCase().includes(filterName)
-        )
-      );
-      if (result.length === 0) return result;
-    }
-
-    if ("sale" in filter) {
-      const sale = sales.find((sale) => sale.id === filter.sale);
-      if (sale) {
-        if (result.length > 0) {
-          exclusionFilter(
-            result,
-            (product) =>
-              !this.productsSaleMap.has(product.id) ||
-              this.productsSaleMap.get(product.id)?.sale.id !== filter.sale
-          );
-        } else {
-          result.push(
-            ...this.select({
-              id: sale.products.map((product) => product.id),
-            }).products
-          );
-        }
-      }
-      if (result.length === 0) return result;
-    }
-
-    if ("category" in filter) {
-      if (result.length > 0) {
-        exclusionFilter(
-          result,
-          (product) => product.category !== filter.category
-        );
-      } else {
-        result.push(
-          ...products.filter((product) => product.category === filter.category)
-        );
-      }
-      if (result.length === 0) return result;
-    }
-
-    if ("minPrice" in filter || "maxPrice" in filter) {
-      const minPrice = filter.minPrice ?? 0;
-      const maxPrice = filter.maxPrice ?? MAX_PRICE;
-      if (result.length > 0) {
-        exclusionFilter(result, (product) => {
-          const productSale = this.productsSaleMap.get(product.id);
-          const productPrice = productSale
-            ? productSale.prices.pix
-            : product.prices.pix;
-          return productPrice < minPrice || productPrice > maxPrice;
-        });
-      } else {
-        result.push(
-          ...products.filter((product) => {
-            const productSale = this.productsSaleMap.get(product.id);
-            const productPrice = productSale
-              ? productSale.prices.pix
-              : product.prices.pix;
-            return productPrice >= minPrice && productPrice <= maxPrice;
-          })
-        );
-      }
-      if (result.length === 0) return result;
-    }
-
-    if ("tags" in filter) {
-      if (result.length > 0) {
-        exclusionFilter(result, (product) => {
-          for (const filterTag of filter.tags as string[]) {
-            if (!product.tags.includes(filterTag)) return true;
-          }
-          return false;
-        });
-      }
-    }
-
-    return result;
-  }
-
-  private injectSales(products: IProduct[]) {
-    for (let i = 0; i < products.length; i++) {
-      const productSale = this.productsSaleMap.get(products[i].id);
-      if (!productSale) return;
-      products[i].sale = productSale.sale;
-      products[i].prices = productSale.prices;
+  public getByQuery(
+    response: FakeAPIResponse<FAProductFormats[]>,
+    query: FAProductQuery,
+  ) {
+    const productsTable = new ProductsTable();
+    productsTable.searchByFilter(query.filter);
+    if (productsTable.empty) return response.setData([]);
+    if (query.sort) productsTable.sort(query.sort);
+    switch (query.format) {
+      case "full":
+        return response.setData(productsTable.getInFullFormat());
+      case "card":
+        return response.setData(productsTable.getInCardFormat());
+      case "suggestion":
+        return response.setData(productsTable.getInSuggestionFormat());
     }
   }
 
-  private selectSale(id: string) {
-    return sales.find((sale) => sale.id === id);
-  }
-
-  public selectSuggestions(search: string) {
-    return this.getProducts({ name: search }).map((product) => ({
-      id: product.id,
-      name: product.name,
-    }));
+  public getRelated(
+    response: FakeAPIResponse<FAProduct_Card[]>,
+    productId: string,
+  ) {
+    const productsTable = new ProductsTable();
+    productsTable.searchById(productId);
+    if (productsTable.empty) return response.setData([]);
+    const product = productsTable.getInFullFormat()[0];
+    productsTable.searchByFilter({ category: product.category });
+    const sameCategoryProducts = productsTable.getInPriceFormat();
+    const relatedProductsIds = sameCategoryProducts
+      .map(({ id, pixPrice }) => ({
+        id,
+        difference: Math.abs(pixPrice - product.prices.pix),
+      }))
+      .sort((difA, difB) => difA.difference - difB.difference)
+      .slice(0, 3)
+      .map(({ id }) => id);
+    productsTable.searchByIdList(relatedProductsIds);
+    response.setData(productsTable.getInCardFormat());
   }
 
   // Temporário
-  public selectRecentlyViewed() {
-    return {
-      products: this.getProducts({
-        id: ["026333169", "688377899", "C45F042A9", "9764E9629"],
-      }),
-    };
-  }
-
-  public selectRelated(baseProductId: string): IProductGroup {
-    const baseProduct = this.getProducts({ id: baseProductId });
-    if (baseProduct.length === 0) return { products: [] };
-    return {
-      products: this.getProducts({
-        category: baseProduct[0].category,
-      })
-        .filter((product) => product.id !== baseProductId)
-        .slice(0, RELATED_ARRAY_MAX_LENGTH),
-    };
-  }
-
-  public select(filter: IFakeApiProductFilter) {
-    const productGroup: IProductGroup = { products: [] };
-    productGroup.products = this.getProducts(filter);
-    this.injectSales(productGroup.products);
-    if ("sale" in filter && typeof filter.sale === "string") {
-      const sale = this.selectSale(filter.sale);
-      if (sale) productGroup.meta = { saleName: sale.name };
-    }
-    if ("sortBy" in filter) {
-      productGroup.products =
-        filter.sortBy === "increasingPrice"
-          ? productGroup.products.sort((a, b) => a.prices.pix - b.prices.pix)
-          : productGroup.products.sort((a, b) => b.prices.pix - a.prices.pix);
-    }
-    return productGroup;
+  public getRecentlyViewed(response: FakeAPIResponse<FAProduct_Card[]>) {
+    const productsTable = new ProductsTable();
+    const idList = [
+      "PRO-026333169",
+      "PRO-688377899",
+      "PRO-002350C9D",
+      "PRO-2107D4DB3",
+    ];
+    productsTable.searchByIdList(idList);
+    return response.setData(productsTable.getInCardFormat());
   }
 }
+
+export { ProductsHandler };
